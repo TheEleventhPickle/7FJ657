c834da0aae94952f0087139d4fcedb50b892bf7a
Daniel Regan <memeception@gmail.com>
2018-06-10 21:42:37 -0500

    added sonic enemy p2

--- js/meme-clicker.js
+++ js/meme-clicker.js
@@ -151,7 +151,7 @@ game.state.add('play', {
 		 * Monsters.
 		 */
 		var monsterData = [
-			{name: 'lucky luciano',      image: 'lucky-luciano',      maxHealth: 10}
+			{name: 'lucky luciano',      image: 'lucky-luciano',      maxHealth: 10},
 			{name: 'sonnnic',      image: 'sonic',      maxHealth: 5}
 		];
 		this.monsters = this.game.add.group();
